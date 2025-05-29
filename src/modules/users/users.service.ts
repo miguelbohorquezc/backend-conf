969f977dbad3d60fe92cda07f7dc7d31fa13101e
Miguel Bohorquez <ingenieriasyltek@gmail.com>
2025-05-29 14:53:56 -0500

    Primer commit del backend

---
+++ src/modules/users/users.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  findAll() {
+    return this.prisma.user.findMany();
+  }
+
+  async create(data: CreateUserDto) {
+    return this.prisma.user.create({
+        data,
+    });
+  }
+}
